2eb8cfe2c39b2d6e9163fd0d1737d3c38f766e5c
aravind <148622715+lomlja@users.noreply.github.com>
2024-10-09 20:00:22 +0400

    Add files via upload

--- js/slide.js
+++ js/slide.js
@@ -2,7 +2,7 @@ let slideIndex = 0;
 let slides = document.querySelectorAll('.slide');
 let videos = document.querySelectorAll('video');
 let backgroundMusic = document.getElementById('backgroundMusic');
-let redirectUrl = 'https://example.com'; // Replace with the desired redirect URL
+let redirectUrl = 'https://lomlja.github.io/ja'; // Replace with the desired redirect URL
 
 // Start playing background music on page load
 backgroundMusic.play();
@@ -11,9 +11,14 @@ backgroundMusic.play();
 showSlide(slideIndex);
 
 function showSlide(n) {
-    // Move the slides if not exceeding bounds
     if (n < slides.length) {
         slides.forEach((slide, i) => {
+            // Add transition effect for all slides except for 8th and 9th (index 7 and 8)
+            if (i === 9 || i === 10 || i === 24 || i === 29  || i === 32 || i === 33 || i === 35 || i === 40 || i === 41 || i === 42) {
+                slide.style.transition = 'none'; // No transition for 8th and 9th slides
+            } else {
+                slide.style.transition = 'transform 0.8s ease-in-out'; // Smooth transition for other slides
+            }
             slide.style.transform = `translateX(-${slideIndex * 100}%)`;
         });
 
@@ -27,9 +32,10 @@ function showSlide(n) {
             activeSlide.muted = false;  // Unmute the video
             activeSlide.play(); // Play the video on the active slide
 
-            // Resume background music when the video ends
+            // Automatically move to the next slide when the video ends
             activeSlide.onended = function() {
-                backgroundMusic.play();
+                backgroundMusic.play(); // Resume background music
+                document.querySelector('.next').click(); // Simulate a click on the "Next" button
             };
         } else {
             // Resume background music if the current slide is not a video
